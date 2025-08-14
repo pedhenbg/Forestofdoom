0da905d9ec91791830628ba17552208a5acc3efa
pedhenbg <pedhenbg@gmail.com>
2025-08-14 10:20:44 -0300

    Adicionar arquivos de projeto.

---
+++ Gameconsole/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gameconsole
+{
+    public class Enemy
+    {
+    public string Name { get; set; }
+    public int Health { get; set; }
+    public int AttackDamage { get; set; }
+
+    public Enemy(string name, int health, int attackDamage)
+        {
+        Name = name;
+        Health = health;
+        AttackDamage = attackDamage;
+        }
+    }
+}
---
+++ Gameconsole/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Gameconsole;
+
+Player player = new Player();
+
+Console.WriteLine("Welcome to Text RPG!");
+
+Console.Write("Enter your character's name: ");
+player.Name = Console.ReadLine();
+player.Health = 100;
+player.AttackDamage = 10;
+
+Console.WriteLine($"\nWelcome, {player.Name}! Your adventure begins now.");
+Console.WriteLine($"Your stats: Health {player.Health}, Attack {player.AttackDamage}");
+Console.WriteLine("\n-------------------------------------\n");
+
+Enemy enemy = new Enemy("Goblin", 50, 5);
+Console.WriteLine($"A wild {enemy.Name} appears!");
+
+while (player.Health > 0 && enemy.Health > 0)
+{
+    Console.Clear();
+
+
+    Console.WriteLine($"\nYour Health: {player.Health} | {enemy.Name}'s Health: {enemy.Health}");
+    Console.WriteLine("Choose your action: (1 - Attack): ");
+    string choice = Console.ReadLine();
+
+    if (choice == "1")
+    {
+        Console.WriteLine("\nPress any key to continue to the next turn...");
+        Console.ReadKey();
+        enemy.Health -= player.AttackDamage;
+        Console.WriteLine($"You attack the {enemy.Name}, dealing {player.AttackDamage} damage");
+    
+    if (enemy.Health <= 0) break;
+
+    player.Health -= enemy.AttackDamage;
+    Console.WriteLine($"The {enemy.Name} attacks you, dealing {enemy.AttackDamage} damage ");
+    }
+    else
+        {
+            Console.WriteLine("Invalid action. You lose your turn!");
+        }
+}
+Console.WriteLine("\n-------------------------------------\n");
+if (player.Health > 0)
+{
+    Console.WriteLine($"Congratulations! You have defeated the {enemy.Name}!");
+}
+else
+{
+    Console.WriteLine($"You have been defeated by the {enemy.Name}. \nGame Over.");
+}
+
+
+    Console.WriteLine("\nPress any key to exit...");
+Console.ReadKey();
